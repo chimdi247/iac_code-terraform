049f7a3c3a5d122df833f640e7b7d91a3c93ee66
chimdi247 <Chimdi247@gmail.com>
2026-02-19 12:14:56 +0200

    first commit

---
+++ terraform/terraform.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = ">= 5.25.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "cloud-chimdi"
+    key    = "terraform.tfstate"
+    region = "eu-west-2"
+  }
+
+  required_version = ">= 1.6.3"
+}
